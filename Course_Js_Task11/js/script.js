887c9d15375334fc925b1caa5ba5653786af36a7
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-14 00:25:44 +0300

    Add files via upload

---
+++ Course_Js_Task11/js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+
+//let a = 0;
+// 1
+//let result1 = (a === 0) ? alert("Correctly") : alert("Wrong");
+// 2
+// let result2 = (a > 0) ? alert("Correctly") : alert("Wrong");
+ // 3
+// let result3 = (a < 0) ? alert("Correctly") : alert("Wrong")
+ // 4
+ //let result4 =  (a >= 0) ? alert("Correctly") : alert("Wrong")
+// 5
+  // let result5 = (a <= 0) ? alert("Correctly") : alert("Wrong")
+ // 6
+ //let result6 = (a != 0) ? alert("Correctly") : alert("Wrong")
+ // 7
+ // let result7 = (a === "test") ? alert("Correctly") : alert("Wrong")
+// 8
+ //let result8 = (a == "1") ? alert("Correctly") : alert("Wrong")
+ // 9
+//let result9 = (a > 0 , a < 5) ? alert("Correctly") : alert("Wrong");
+
+
+// 10
+//let a = 0;
+//let result = (a === 0 || a === 2) ? (a + 7) : (a / 10);
+//alert(result);
+
+// 11
+//let a = 10;
+//let b = 2;
+//let result = (a <= 1, b >= 3) ? alert(a + b) : alert(a - b);
+
+
+// 12
+//let a = 15;
+//let b = 15;
+//let result = (a > 2 && a < 11 || b >= 6 && b < 14) ? alert("Correctly") : alert("Wrong");
+
+
+// 13
+/*
+let num = '1';
+let result;
+switch (num) {
+  case '1':
+    result = "Winter";
+    break;
+  case '2':
+    result = "Spring";
+    break;
+  case '3':
+    result = "Summer";
+    break;
+  case '4':
+    result = "Autumn";
+    break;
+}
+alert(result);
+*/
